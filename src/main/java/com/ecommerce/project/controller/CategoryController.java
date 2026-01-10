c1a734a5c86450b080cbf99ac45c60821392b08e
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-01-10 09:47:48 +0700

    Add category creation

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,22 +1,26 @@
 package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Category;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/public")
+@RequestMapping("/api")
 public class CategoryController {
 
     private List<Category> categories = new ArrayList<>();
 
-    @GetMapping("/categories")
+    @GetMapping("/public/categories")
     public List<Category> getAllCategories() {
         return categories;
+    }
+
+    @PostMapping("/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        if (category != null) return "Category addition successful";
+        else return "Category addition unsuccessful";
     }
 
 }
