9aa88a81d08ad693c5b604eb8ed4cbc3042991ee
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-01-16 19:29:36 +0700

    Update category service

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -13,22 +15,25 @@ import java.util.Optional;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
-    private List<Category> categories = new ArrayList<>();
+    // private List<Category> categories = new ArrayList<>();
     //private Long nextId = 1L;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @Override
     public List<Category> getAllCategories() {
-        return categories;
+        return categoryRepository.findAll();
     }
 
     @Override
     public void createCategory(Category category) {
-        if (categories.isEmpty()) {
+        if (categoryRepository.findAll().isEmpty()) {
             category.setCategoryId(1L);
-            categories.add(category);
+            categoryRepository.save(category);
         }else {
-            category.setCategoryId(categories.getLast().getCategoryId() + 1);
-            categories.add(category);
+            category.setCategoryId(categoryRepository.findAll().getLast().getCategoryId() + 1);
+            categoryRepository.save(category);
         }
 //        category.setCategoryId(nextId++);
 //        categories.add(category);
@@ -36,6 +41,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public String deleteCategory(Long categoryId) {
+         List<Category> categories = categoryRepository.findAll();
         Category category = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst()
@@ -43,12 +49,13 @@ public class CategoryServiceImpl implements CategoryService {
                         new ResponseStatusException(HttpStatus.NOT_FOUND, "Category with categoryId "
                                 + categoryId + " is not found"));
 
-        categories.remove(category);
+        categoryRepository.delete(category);
         return "Category with categoryId " + categoryId + " is deleted successfully";
     }
 
     @Override
     public String updateCategory(Long categoryId, Category category) {
+        List<Category> categories = categoryRepository.findAll();
         Optional<Category> optionalCategory = categories.stream()
                 .filter(c -> c.getCategoryId().equals(categoryId))
                 .findFirst();
