262a523aefe84e5252586c569e4104f03dac0a94
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-01-28 15:10:10 +0700

    Add getAllProducts function

--- src/main/java/com/ecommerce/project/controller/ProductController.java
+++ src/main/java/com/ecommerce/project/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.project.controller;
 
 import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
 import com.ecommerce.project.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -19,5 +20,10 @@ public class ProductController {
                                                  @PathVariable Long categoryId){
         ProductDTO productDTO = productService.addProduct(categoryId, product);
         return new ResponseEntity<>(productDTO, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/public/products")
+    public ResponseEntity<ProductResponse> getAllProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
     }
 }
--- src/main/java/com/ecommerce/project/service/ProductService.java
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -2,8 +2,10 @@ package com.ecommerce.project.service;
 
 import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.ProductDTO;
-import org.springframework.stereotype.Service;
+import com.ecommerce.project.payload.ProductResponse;
 
 public interface ProductService {
     ProductDTO addProduct(Long categoryId, Product product);
+
+    ProductResponse getAllProducts();
 }
