0adb21815eab75042f8b030b78e4d9de7699080e
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-15 22:53:10 +0700

    Add RoleRepository

--- src/main/java/com/ecommerce/project/repositories/UserRepository.java
+++ src/main/java/com/ecommerce/project/repositories/UserRepository.java
@@ -1,9 +1,6 @@
 package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
