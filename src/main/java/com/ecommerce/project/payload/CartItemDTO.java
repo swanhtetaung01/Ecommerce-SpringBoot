732195bef68dd51dc90a90d190084b8fba216d7c
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-19 22:33:46 +0700

    Add cartRepo and cartItemRepo

--- src/main/java/com/ecommerce/project/payload/CartItemDTO.java
+++ src/main/java/com/ecommerce/project/payload/CartItemDTO.java
@@ -1,8 +1,12 @@
 package com.ecommerce.project.payload;
 
-import com.ecommerce.project.model.Cart;
-import com.ecommerce.project.model.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CartItemDTO {
     private Long cartItemId;
     private CartDTO cart;
