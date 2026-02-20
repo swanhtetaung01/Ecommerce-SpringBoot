8bb8fef9aaf4c0f58c4ffe861fa7c2fc55b8aab8
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-20 22:39:03 +0700

    Using cartItemRepo in cartServiceImpl

--- src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -2,6 +2,9 @@ package com.ecommerce.project.repositories;
 
 import com.ecommerce.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    @Query("SELECT ci FROM CartItem ci WHERE ci.product.productId = ?1 AND ci.cart.cartId = ?2")
+    CartItem findCartItemByProductIdAndCartId(Long productId, Long cartId);
 }
--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -1,8 +1,13 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Cart;
+import com.ecommerce.project.model.CartItem;
+import com.ecommerce.project.model.Product;
 import com.ecommerce.project.payload.CartDTO;
+import com.ecommerce.project.repositories.CartItemRepository;
 import com.ecommerce.project.repositories.CartRepository;
+import com.ecommerce.project.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,21 +17,39 @@ public class CartServiceImpl implements CartService {
     @Autowired
     private CartRepository cartRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CartItemRepository cartItemRepository;
+
+    @Autowired
+    private AuthUtil authUtil;
+
     @Override
     public CartDTO addProductToCart(Long productId, Integer quantity) {
         //Find an existing cart or create one
-        Cart userCart = cartRepository.
-        if(userCart == null) {
-            Cart newCart = new Cart();
-            cartRepository.save(newCart);
-        }else {
-            userCart = cartRepository.findAll();
-        }
+        Cart cart = getCart();
         //Retrieve Product Details
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
         //Perform validations about the product
+        CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(
+                productId,
+                cart.getCartId()
+                );
         //Create a cart item
         //save cart item into the cart
         //return the updated cart
         return null;
+    }
+    private Cart getCart(){
+        Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInEmail());
+        if(userCart != null)
+            return userCart;
+        Cart newCart = new Cart();
+        newCart.setTotalPrice(0.00);
+        newCart.setUser(authUtil.loggedInUser());
+        return cartRepository.save(newCart);
     }
 }
