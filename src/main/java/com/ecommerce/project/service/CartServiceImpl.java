e8cf9f11708ae215e582acda9cd840222415d61f
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-20 23:58:50 +0700

    Update cartServiceImpl

--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.project.service;
 
+import com.ecommerce.project.exceptions.APIException;
 import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Cart;
 import com.ecommerce.project.model.CartItem;
@@ -34,13 +35,32 @@ public class CartServiceImpl implements CartService {
         Product product = productRepository.findById(productId)
                 .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
         //Perform validations about the product
+        if(product.getQuantity() == 0){
+            throw new APIException(product.getProductName() + " is out of stock");
+        }else if(product.getQuantity() < quantity && product.getQuantity() == 1) {
+            throw new APIException("Only 1 item left in the stock");
+        }else if(product.getQuantity() < quantity){
+            throw new APIException("Only " + product.getQuantity() + " items left in the stock");
+        }
         CartItem cartItem = cartItemRepository.findCartItemByProductIdAndCartId(
                 productId,
                 cart.getCartId()
                 );
-        //Create a cart item
+        if(cartItem != null && product.getQuantity() != 0){
+            cartItem.setQuantity(cartItem.getQuantity() + quantity);
+        }
+        cartItem.setQuantity(quantity);
+        cartItem.setProduct(product);
+        cartItem.setProductPrice(product.getPrice());
+        cartItem.setDiscount(product.getDiscount());
+        cartItem.setCart(cart);
+        //Create a cartItem
         //save cart item into the cart
+        cartItemRepository.save(cartItem);
+        product.setQuantity(product.getQuantity());
         //return the updated cart
+        cart.setTotalPrice(cart.getTotalPrice() + (product.getSpecialPrice() * quantity));
+        cartRepository.save(cart);
         return null;
     }
     private Cart getCart(){
