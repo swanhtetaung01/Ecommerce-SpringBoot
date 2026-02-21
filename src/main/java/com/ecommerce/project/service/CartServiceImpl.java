c6ddab61d2fb021a597b3351e2e126801829782a
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-02-21 16:46:02 +0700

    Add getAllCarts endpoint and implementation

--- src/main/java/com/ecommerce/project/service/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CartServiceImpl.java
@@ -16,6 +16,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
@@ -89,6 +91,24 @@ public class CartServiceImpl implements CartService {
         cartDTO.setProducts(productStream.toList());
         return cartDTO;
     }
+
+    @Override
+    public List<CartDTO> getAllCarts() {
+        List<Cart> carts = cartRepository.findAll();
+
+        if(carts.isEmpty()) {
+            throw new APIException("No cart exist");
+        }
+
+        return carts.stream().map(cart -> {
+            CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
+            List<ProductDTO> products = cart.getCartItems().stream().map(item ->
+                    modelMapper.map(item.getProduct(), ProductDTO.class)).toList();
+            cartDTO.setProducts(products);
+            return cartDTO;
+        }).toList();
+    }
+
     private Cart getCart(){
         Cart userCart = cartRepository.findCartByEmail(authUtil.loggedInUserEmail());
         if(userCart != null)
