84a4c458b53c4b13e5431d52ce709d4226115dc6
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-02 00:17:33 +0700

    Add DTOs and Repositories

---
+++ src/main/java/com/ecommerce/project/payload/OrderDTO.java
@@ -0,0 +1,30 @@
+package com.ecommerce.project.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderDTO {
+
+    private Long orderId;
+
+    private String email;
+
+    private List<OrderItemDTO> orderItems;
+
+    private LocalDate orderDate;
+
+    private Double totalAmount;
+
+    private String orderStatus;
+
+    private PaymentDTO payment;
+
+    private Long addressId;
+}
