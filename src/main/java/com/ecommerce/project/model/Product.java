57b870251bf2fe10abc50fbb3995d0fd7e946981
Swan Htet Aung <137768986+swanhtetaung01@users.noreply.github.com>
2026-03-11 15:52:25 +0700

    Add ER Diagram and fix some loose ends

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -20,7 +20,7 @@ import java.util.List;
 @ToString
 public class Product {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
 
     @NotBlank
